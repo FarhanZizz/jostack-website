e5280e2c17e0a0eee8617551ee8632be9056429d
FarhanZizz <iamfarhanaziz@gmail.com>
2024-09-04 20:02:08 +0600

    Routes Setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import { Outlet } from "react-router-dom";
+
 function App() {
-  return <div className="App lg:max-w-[80%] mx-auto"></div>;
+  return (
+    <div className="App lg:max-w-[80%] mx-auto">
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Routes/routes.js
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Home from "../Pages/Home";
+import Projects from "../Pages/Projects";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/projects",
+        element: <Projects />,
+      },
+    ],
+  },
+]);
+
+export default routes;

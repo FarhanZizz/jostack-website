dcb522e8f21a9c833d77295d62ceefa581096e56
Farhan Aziz <iamfarhanaziz@gmail.com>
2024-10-26 10:11:15 -0700

    About Section Complete

--- src/Components/About.js
+++ src/Components/About.js
@@ -21,7 +21,7 @@ const About = () => {
       ".a-one",
       {
         opacity: "1",
-        marginTop: "-25%",
+        marginTop: "-35%",
         duration: 3,
         delay: 2,
       },
@@ -64,7 +64,7 @@ const About = () => {
       })
 
       .to(".about-title", {
-        marginLeft: "80%",
+        marginLeft: "70%",
         duration: 10,
         delay: 2,
       });
@@ -73,51 +73,42 @@ const About = () => {
   return (
     <div className=" about-section  bg-[#ffffff]    h-[100vh]  flex  rounded-xl">
       {/* left */}
-      <div className="right h-[100%] p-10   w-[50%]">
-        <span className="text-black text-[3vw] about-title grotesk font-bold capitalize  mt-[40%] underline w-[80%]  flex justify-center items-center gap-4 about-title ">
-          how jostack work <BsFillPatchQuestionFill />{" "}
+      <div className="right h-[100%] ml-20   w-[50%]">
+        <span className="text-black text-[3vw] about-title grotesk font-bold capitalize  mt-[40%]  w-[80%]  flex justify-center items-center gap-4 about-title ">
+          how jostack works <BsFillPatchQuestionFill />{" "}
         </span>
       </div>
       {/* <span className='text-white text-[6vw] mt-[25%] question '></span> */}
 
       {/* right */}
       <div className="left h-[100%] w-[50%]  ml-44 mt-[30%]">
-        <div className="w-[60%]  a-one opacity-0">
+        <div className="w-[70%]  a-one opacity-0 mt-20">
           <h1 className="text-black text-[3vw] capitalize mb-4  grotesk  ">
-            TItle 1
+            Discover and Plan
           </h1>
           <div className=" h-[0.5px] w-full bg-primary rounded-xl"></div>
           <p className="text-black text-[20px] capitalize mt-5">
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a more-or-less normal
-            distribution
+            We begin by understanding your business needs and goals. From There, we create a tailored plan, outlining the digital tools and services that will best suit your brand.
           </p>
         </div>
 
-        <div className="a-two opacity-0 w-[60%] mt-20">
+        <div className="a-two opacity-0 w-[70%] mt-20">
           <h1 className="text-black text-[3vw] capitalize mb-4  grotesk ">
-            Title 2
+            Design and Develop
           </h1>
           <div className=" h-[0.5px] w-full bg-black rounded-xl"></div>
           <p className="text-black text-[20px] capitalize mt-5">
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a more-or-less normal
-            distribution
+           Our team designs and builds your project, focusing on seamless functionality, responsive design, and user experience. Every detail is crafted to align with your vision and objectives.
           </p>
         </div>
 
-        <div className="a-three opacity-0 w-[60%] mt-20">
+        <div className="a-three opacity-0 w-[70%] mt-20">
           <h1 className="text-black text-[3vw] capitalize mb-4  grotesk   ">
-            Title 3
+            Launch and Support
           </h1>
           <div className=" h-[0.5px] w-full bg-black rounded-xl"></div>
           <p className="text-black text-[20px] capitalize mt-5">
-            It is a long established fact that a reader will be distracted by
-            the readable content of a page when looking at its layout. The point
-            of using Lorem Ipsum is that it has a more-or-less normal
-            distribution
+           Once reviewed and approved, we launch your project with ongoing support. We monitor project performance, provide updates, and offer services that help you grow and succeed.
           </p>
         </div>
       </div>

90a94c1bb58c8dbe936f1e0844493d4a4978803b
Farhan Aziz <iamfarhanaziz@gmail.com>
2024-12-18 23:38:28 +0600

    made footer responsive

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -47,7 +47,7 @@ const Footer = () => {
   const isContactRoute = location.pathname === "/contact";
   return (
     <div
-      className={` grotesk lg:p-20 p-5  ${
+      className={` grotesk lg:p-20 py-20 px-5  ${
         isProjectsRoute || isContactRoute
           ? "bg-[#141517] text-white -mt-1"
           : "bg-white text-black mt-7"
@@ -58,7 +58,7 @@ const Footer = () => {
       <svg
         onMouseMove={handleMouseMove}
         onMouseLeave={handleMouseLeave}
-        className="lea"
+        className="lea hidden lg:block"
         width="100%" // Ensure full width
         height="300px" // Fixed height of 400px
         viewBox="0 0 1800 300" // Increased the viewBox width for a longer path
@@ -67,19 +67,19 @@ const Footer = () => {
           ref={pathRef}
           d={path}
           fill="transparent"
-          stroke="black"
+          stroke={isProjectsRoute || isContactRoute ? "#fff" : "#000"}
           strokeWidth="2"
         />
       </svg>
 
       {/*  */}
 
-      <div className="lg:flex flex-col ">
+      <div className="">
         <h1 className="lg:text-5xl text-3xl font-bold grotesk ">
           Contact Information
         </h1>
       </div>
-      <div className="lg:flex justify-between lg:space-y-0 space-y-7  mt-9">
+      <div className="lg:flex justify-between items-center lg:space-y-0 space-y-7  mt-9">
         <div className="text-3xl">
           <h1>Socials</h1>
           <div className="flex gap-4 mt-4 ">
@@ -89,7 +89,7 @@ const Footer = () => {
         </div>
         <div>
           <h1 className="text-3xl mb-3">Email</h1>
-          <code className="text-2xl">jostackservices@gmail.com</code>
+          <code className="text-xl lg:text-2xl">jostackservices@gmail.com</code>
         </div>
         <div>
           <CustomLink
@@ -98,11 +98,11 @@ const Footer = () => {
               isProjectsRoute || isContactRoute
                 ? "border-white text-white hover:border-primary hover:bg-primary"
                 : "btn-primary"
-            } text-2xl w-full`}
+            } text-xl lg:text-2xl w-full`}
           >
             Get In Touch
           </CustomLink>
-          <p className="text-lg mt-2">
+          <p className="lg:text-lg mt-2">
             © 2024 Jostack Services. All rights reserved
           </p>
         </div>

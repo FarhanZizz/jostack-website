54edf248fcefae2e941cbdf090520d54109f848f
Farhan Aziz <iamfarhanaziz@gmail.com>
2024-12-19 20:48:16 +0600

    footer sizing fix

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -75,7 +75,7 @@ const Footer = () => {
       {/*  */}
 
       <div className="">
-        <h1 className="lg:text-5xl text-3xl font-bold grotesk ">
+        <h1 className="lg:text-6xl text-3xl font-bold grotesk ">
           Contact Information
         </h1>
       </div>

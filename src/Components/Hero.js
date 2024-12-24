ff37e2cb164c618f3cc098451f1cecbe14cdc382
Farhan Aziz <iamfarhanaziz@gmail.com>
2024-12-24 12:46:28 +0600

    made hero responsive

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -4,8 +4,8 @@ import gsap from "gsap";
 import Lottie from "react-lottie";
 import animationData from "../assets/circle.json";
 import React from "react";
-// import Lottie from "react-lottie";
-// import animationData from "../assets/circle.json";
+import CustomLink from "./CustomLink";
+
 const Hero = () => {
   useGSAP(() => {
     gsap.from(".hero-title h1 ", {
@@ -45,38 +45,31 @@ const Hero = () => {
   };
 
   return (
-    <div className="">
-      <div className="hero-section max-w-[1500px] mx-auto lg:flex justify-between  min-h-screen lg:mt-44
-      mt-20  ">
-        <div className=" hero-title lg:text-left text-center grotesk ">
-          <h1 className="lg:text-9xl text-5xl font-bold "> TRANSFORMING IDEAS </h1>
-          <h1 className="lg:text-9xl text-5xl font-bold  ">INTO DIGITAL </h1>
-          <h1 className="lg:text-9xl text-5xl font-bold  ">REALITY</h1>
-        </div>
+    <div className="lg:flex justify-between lg:px-20 lg:py-20 2xl:py-28  px-5">
+      <div className="hero-title lg:text-left text-center grotesk ">
+        <h1 className="lg:text-8xl 2xl:text-9xl text-5xl font-bold uppercase">
+          TRANSFORMING IDEAS <br /> INTO DIGITAL <br /> REALITY
+        </h1>
+      </div>
 
-        <div className="hero-details   text-[14px] space-y-2">
-          <div className="svg lg:mb-32 m-10">
-            <Lottie options={defaultOptions} height={250} width={250} />
-          </div>
-          <div className="text-[20px] lg:text-left text-center lg:p-0 p-4 max-w-xl capitalize">
-            <p>
-              At Jostack, we transform your ideas into digital reality with
-              expert website
-            </p>
-            <p>
-              creation, management and branding. Explore our wide range of IT
-              services
-            </p>
-            <p> designed to elevate your business in the digital world.</p>
-          </div>
-          <div className="flex items-center lg:justify-normal justify-center lg:mt-4">
-            <a
-              href="/contact"
-              className="btn-hero rounded-3x bg-primary px-3 py-1 rounded-xl text-white flex items-center gap-3"
-            >
-              Contact <FaArrowAltCircleRight />{" "}
-            </a>
-          </div>
+      <div className="hero-details">
+        <div className="svg lg:mb-20 2xl:w-[250px] 2xl:h-[250px] h-[225px] w-[225px] mx-auto">
+          <Lottie options={defaultOptions} />
+        </div>
+        <div className="text-xl lg:text-left text-center lg:p-0 p-4 max-w-xl capitalize">
+          <p>
+            At Jostack, we transform your ideas into digital reality with expert
+            website creation, management and branding. Explore our wide range of
+            IT services designed to elevate your business in the digital world.
+          </p>
+        </div>
+        <div className="flex items-center lg:justify-normal justify-center lg:mt-4">
+          <CustomLink
+            to="/contact"
+            className="btn-hero rounded-3x bg-primary px-3 py-1 rounded-xl text-white flex items-center gap-3"
+          >
+            Contact <FaArrowAltCircleRight />{" "}
+          </CustomLink>
         </div>
       </div>
     </div>

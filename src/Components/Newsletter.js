131f06cb3ecbb7f5f9e0898906cdb329c47e8af1
Farhan Aziz <iamfarhanaziz@gmail.com>
2025-01-08 21:48:17 +0600

    Newletter made responsive

--- src/Components/Newsletter.js
+++ src/Components/Newsletter.js
@@ -10,7 +10,7 @@ const Newsletter = () => {
         scroller: ".smooth-scroll",
         start: "-10% 70%",
         end: "50% 50%",
-        // markers:true
+        // markers: true,
       },
     });
 
@@ -37,22 +37,24 @@ const Newsletter = () => {
   });
 
   return (
-    <div className="Newsletter mx-auto mb-10 mt-10 max-w-[1700px] rounded-xl bg-black p-12">
-      <section className="content items-center justify-between text-white lg:flex">
-        <h1 className="grotesk Text text-2xl font-semibold uppercase lg:text-[3vw]">
-          Want to get in touch ?
-        </h1>
-        <div className="input mt-12 flex items-center gap-3 lg:mt-0 lg:p-10">
-          <input
-            placeholder="email@gmail.com"
-            required
-            className="grotesk w-96 rounded-2xl border-2 border-white bg-[#f1f1f1fb] p-3 px-10 text-[20px] text-[#000]"
-          ></input>
-          <button className="grotesk rounded-xl bg-[#8768b77d] px-6 py-3 text-[1vw] font-semibold uppercase text-white delay-100 duration-100 hover:bg-primary hover:px-10 hover:text-white">
-            Subscribe
-          </button>
-        </div>
-      </section>
+    <div className="mt-16 px-5 lg:px-20">
+      <div className="Newsletter rounded-xl bg-black p-5 lg:p-10">
+        <section className="content items-center justify-between text-white lg:flex">
+          <h1 className="grotesk Text text-2xl font-semibold uppercase lg:text-[3vw]">
+            Want to get in touch ?
+          </h1>
+          <div className="input mt-8 flex items-center p-0 lg:mt-0 lg:p-10">
+            <input
+              placeholder="email@gmail.com"
+              required
+              className="grotesk w-full rounded-xl border-2 border-white bg-[#f1f1f1fb] pl-2 text-[20px] text-[#000]"
+            ></input>
+            <button className="grotesk rounded bg-[#8768b77d] px-6 py-3 text-base font-semibold uppercase text-white delay-100 duration-100 hover:bg-primary hover:px-10 hover:text-white lg:rounded-xl">
+              Subscribe
+            </button>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
